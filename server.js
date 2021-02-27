c544a13cc326c3af597f10273ecc01080bf06bda
dl-watson <dlarkinwatson@gmail.com>
2021-02-27 07:40:14 -0800

    initialized new node project, set up express server and a basic hello world graphql application

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./lib/app");
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`Started on ${PORT}`);
+});
+
+process.on("exit", () => {
+  console.log("Goodbye!");
+});
